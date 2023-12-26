2522c33309e5c6c1352c181986fa3354963236ab
TkTnX <timur20-09@mail.ru>
2023-12-26 17:21:49 +0500

    SELHONO V4
    Добавлена новая страница "About us". В ней секция слайдера, цитата и различная информация.

--- selhono/js/main.js
+++ selhono/js/main.js
@@ -7,3 +7,32 @@ headerBurgerBtn.addEventListener("click", () => {
   headerBurger.classList.toggle("header__burger-active");
   bodyLock.classList.toggle("lock");
 });
+
+// * Design Slider
+
+const swiper = new Swiper(".design__swiper", {
+  // Optional parameters
+  spaceBetween: 17,
+  loop: true,
+  slidesPerView: 1,
+
+  autoplay: {
+    delay: 3000,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
